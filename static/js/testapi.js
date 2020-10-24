74a0a5284b11ee91ca790210c27415c594d8d250
Becki Oeswein Leeds <rmoesw01@gmail.com>
2020-10-24 15:48:21 -0400

    started globe visualization

--- static/js/testapi.js
+++ static/js/testapi.js
@@ -1,27 +1,66 @@
+// FRTH = Thickness of frozen ground layer
+// TMAX - Annual Mean Maximum Temperature
+// PRCP - Total Annual precipitation
+// SNOW - Total Annual Snowfall in millimeters
+// MXyy - Annual mean of daily maximum soil temperature.
+
+// Datasets
+// GHCND Daily Summaries
+// GSOM Global Summary of the Month
+// GSOY Global Summary of the Year
+// NEXRAD2 Weather Radar (Level II)
+// NEXRAD3 Weather Radar (Level III)
+// NORMAL_ANN Normals Annual/Seasonal
+// NORMAL_DLY Normals Daily
+// NORMAL_HLY Normals Hourly
+// NORMAL_MLY Normals Monthly
+// PRECIP_15 Precipitation 15 Minute
+// PRECIP_HLY Precipitation Hourly
+
 console.log("Hello World")
-$.ajax({ 
-    url:"https://www.ncdc.noaa.gov/cdo-web/api/v2/data", 
-    data:{
-        datasetid: "GSOY",
-        datatypeid:"TMAX",
-        // datatypeid:"TMIN",
-        // datatypeid: "EMXP" + "&" + "PRCP",
-        // datatypeid: "TMAX"&"TMIN"&TAVG&EMXT&EMNT&DX90&DX70&DX32&DT32&DT00&PRCP&EMXP&SNOW&EMSN&EVAP&AWND&WSFM&WSFG",
-        // sortfield: 'mindate',
-        limit: 250,
-        startdate: "1880-01-01",
-        enddate: "1881-01-01"
-        // startdate: "2000-01-01",
-        // enddate: "2001-01-01"
-    }, 
-    headers:{ 
-        token:NOAA_token
-    } }).then(data => {
-        table1 = d3.json(data.results)
+$.ajax({
+    url: "https://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes",
+    data: {
+        // datasetid: "GHCND",
+        // // datatypeid: ("TMAX","TMIN"),
+        // datatypeid:"FRTH",
+        // // datatypeid: "EMXP" + "&" + "PRCP",
+        // // datatypeid: "TMAX"&"TMIN"&TAVG&EMXT&EMNT&DX90&DX70&DX32&DT32&DT00&PRCP&EMXP&SNOW&EMSN&EVAP&AWND&WSFM&WSFG",
+        // // sortfield: 'mindate',
+        // stationid: "GHCND:AGE00147708",
+        limit: 1000,
+        // // startdate: "2018-01-01",
+        // // enddate: "2018-04-01"
+        // startdate: "1960-01-01",
+        // enddate: "1960-12-01"
+    },
+    headers: {
+        token: NOAA_token
+    }
+}).then(data => {
+    console.log(data.results)
+    // for (i=0; i<11; i++){
+    //     console.log(data.results[i].id, data.results[i].name)
+    // }
+    // table1 = d3.json(data.results)
 
-    })
+})
+console.log("Goodbye World")
 
-// $.ajax({ 
+// $.ajax({
+//     url: "https://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes",
+//     data: {
+//         limit: 200,
+//         datasetid: "GSOY"
+
+//     },
+//     headers: {
+//         token: NOAA_token
+//     }
+// }).then(data => {
+//     console.log(data.results)
+// })
+// $.ajax({
 //     url:"https://www.ncdc.noaa.gov/cdo-web/api/v2/stations", 
 //     data:{
 //         limit: 1000,
@@ -36,11 +75,7 @@ $.ajax({
 
 //     })
 
-// FRTH = Thickness of frozen ground layer
-// TMAX - Annual Mean Maximum Temperature
-// PRCP - Total Annual precipitation
-// SNOW - Total Annual Snowfall in millimeters
-// MXyy - Annual mean of daily maximum soil temperature.
+
 
 
 
