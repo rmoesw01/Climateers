a2097b6596de0f0c7b0e41e51b5eab52303779b7
Maria Dong <maria.j.dong@gmail.com>
2020-10-25 19:10:35 -0400

    add N ice cap morph from 1979 to 2016 (only 2 years for now)

--- maria/sea_ice/sea_ice_copy.js
+++ maria/sea_ice/sea_ice_copy.js
@@ -1,12 +1,6 @@
-// retrieve geoJSON data for sea ice extent
-// for (var x = 1979; x < 2017; x++) {
-//     var url = `https://raw.githubusercontent.com/vigorousnorth/arctic-ice/master/geojson_files/extent_N_${x}08_geo.json`;
-// }
-
 am4core.ready (function() {
     // standard animated theme
     am4core.useTheme(am4themes_animated);
-    // am4core.useTheme(am4themes_amchartsdark);
     
     // create map instances
     var chart = am4core.create("chartdiv", am4maps.MapChart);
@@ -20,6 +14,7 @@ am4core.ready (function() {
     chart.projection = new am4maps.projections.Orthographic();
     chart.panBehavior = "rotateLongLat";
     chart2.projection = new am4maps.projections.Orthographic();
+    chart2.panBehavior = "rotateLongLat";
 
     // set bg color
     chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#2E4053");
@@ -29,9 +24,11 @@ am4core.ready (function() {
     
     // create map polygon series for countries
     var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    var polygonSeries2 = chart2.series.push(new am4maps.MapPolygonSeries());
     
     // load polygon geodata from geoJSON
     polygonSeries.useGeodata = true;
+    polygonSeries2.useGeodata = true;
     
     // configure template (tooltip & fill/strokes)
     var polygonTemplate = polygonSeries.mapPolygons.template;
@@ -61,8 +58,6 @@ am4core.ready (function() {
 
     // config fill, stroke, color
     var iceTemplate = iceSeries.mapPolygons.template;
-
-    console.log (iceSeries.mapPolygons);
     // iceTemplate.tooltipText = "{name}";
     iceTemplate.fill = am4core.color("rgba(255, 255, 255, 1.0)");
     iceTemplate.fillOpacity = 0.9;
@@ -84,67 +79,91 @@ am4core.ready (function() {
     graticuleSeries.fitExtent = false;
     graticuleSeries.fill = am4core.color("rgba(255, 255, 255, 1.0)");
 
-    var slider = chart.createChild(am4core.Slider);
-    slider.padding(0, 15, 0, 60);
-    slider.background.padding(0, 15, 0, 60);
-    slider.marginBottom = 15;
-    slider.valign = "bottom";
+    chart.events.on ('ready', function() {
+      setTimeout (function() {
+        morphToPolygon();
+      }, 3000)
+    });
 
-    var currentIndex = -1;
-    // var colorset = new am4core.ColorSet();
-
-    setInterval (function () {
-        var next = slider.start + 1 / (2016 - 1979 + 1);
-        if (next >= 1) {
-            next = 0;
-        }
-
-        slider.animate ({ property: "start", to: next }, 300);
-        }, 2000)
-
-    slider.events.on("rangechanged", function () {
-        changeYear();
-    })
-    
-    function changeYear() {
-        var totalYears = 2016 - 1979;
-        var yearIndex = Math.round(totalYears * slider.start);
-      
-        var morphToPolygon;
-      
-        if (currentIndex != yearIndex) {
-
-        //   console.log (currentIndex, yearIndex);
-          iceSeries2.data = [];
-          iceSeries2.geodataSource.url = `https://raw.githubusercontent.com/vigorousnorth/arctic-ice/master/geojson_files/extent_N_${1979+yearIndex}08_geo.json`
-        //   console.log (iceSeries2.geodataSource.url);
-          
-          currentIndex = yearIndex;
-      
-          iceSeries2.events.once ("validated", function() {
-            console.log ('validated!');
-            morphToPolygon = iceSeries2.mapPolygons.getIndex(0);
-            console.log (morphToPolygon);
-
-            if (morphToPolygon) {
-              console.log ('again');
-              var icePolygon = iceSeries.mapPolygons.getIndex(0);
-      
-              var morpher = icePolygon.polygon.morpher;
-              var morphAnimation = morpher.morphToPolygon(morphToPolygon.polygon.points);
-      
-            //   var colorAnimation = icePolygon.animate({ "property": "fill", "to": colorset.getIndex(Math.round(Math.random() * 20)) }, 1000);
-      
-            //   var animation = label.animate({ property: "y", to: 1000 }, 300);
-      
-            //   animation.events.once("animationended", function () {
-            //     label.text = morphToPolygon.dataItem.dataContext["name"];
-            //     label.y = -50;
-            //     label.animate({ property: "y", to: 200 }, 300, am4core.ease.quadOut);
-            //   })
-            }
-          })
-        }
+    function morphToPolygon() {
+      var polygon1 = iceSeries.mapPolygons.getIndex(0);
+      var polygon2 = iceSeries2.mapPolygons.getIndex(0);
+      var animation = polygon1.polygon.morpher.morphToPolygon (polygon2.polygon.points);
+      animation.events.on ('animationended', function() {
+        setTimeout (morphBack, 2000);
+      });
     }
+
+    function morphBack() {
+      var polygon = iceSeries.mapPolygons.getIndex(0);
+      polygon.polygon.morpher.morphBack();
+
+      setTimeout (function() {
+        morphToPolygon();
+      }, 3000)
+    }
+
+    // var slider = chart.createChild(am4core.Slider);
+    // slider.padding(0, 15, 0, 60);
+    // slider.background.padding(0, 15, 0, 60);
+    // slider.marginBottom = 15;
+    // slider.valign = "bottom";
+
+    // var currentIndex = -1;
+    // // var colorset = new am4core.ColorSet();
+
+    // setInterval (function () {
+    //     var next = slider.start + 1 / (2016 - 1979 + 1);
+    //     if (next >= 1) {
+    //         next = 0;
+    //     }
+
+    //     slider.animate ({ property: "start", to: next }, 300);
+    //     }, 2000)
+
+    // slider.events.on("rangechanged", function () {
+    //     changeYear();
+    // })
+    
+    // function changeYear() {
+    //     var totalYears = 2016 - 1979;
+    //     var yearIndex = Math.round(totalYears * slider.start);
+      
+    //     var morphToPolygon;
+      
+    //     if (currentIndex != yearIndex) {
+
+    //     //   console.log (currentIndex, yearIndex);
+    //       iceSeries2.data = [];
+    //       iceSeries2.geodataSource.url = `https://raw.githubusercontent.com/vigorousnorth/arctic-ice/master/geojson_files/extent_N_${1979+yearIndex}08_geo.json`
+    //     //   console.log (iceSeries2.geodataSource.url);
+          
+    //       currentIndex = yearIndex;
+      
+    //       iceSeries2.events.once ("validated", function() {
+    //         console.log ('validated!');
+    //         morphToPolygon = iceSeries2.mapPolygons.getIndex(0);
+    //         console.log (morphToPolygon);
+
+    //         if (morphToPolygon) {
+    //           console.log ('again');
+    //           var icePolygon = iceSeries.mapPolygons.getIndex(0);
+      
+    //           var morpher = icePolygon.polygon.morpher;
+    //           var morphAnimation = morpher.morphToPolygon(morphToPolygon.polygon.points);
+      
+    //         //   var colorAnimation = icePolygon.animate({ "property": "fill", "to": colorset.getIndex(Math.round(Math.random() * 20)) }, 1000);
+      
+    //         //   var animation = label.animate({ property: "y", to: 1000 }, 300);
+      
+    //         //   animation.events.once("animationended", function () {
+    //         //     label.text = morphToPolygon.dataItem.dataContext["name"];
+    //         //     label.y = -50;
+    //         //     label.animate({ property: "y", to: 200 }, 300, am4core.ease.quadOut);
+    //         //   })
+    //         }
+    //       })
+    //     }
+    // }
 
 }); // end am4core.ready()
