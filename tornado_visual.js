45d988c69d48883b38b65f32f594e3c9e9aac5d9
AmberP <63465031+apizzo1@users.noreply.github.com>
2020-10-27 15:48:23 -0400

    adding tornado viz

---
+++ tornado_visual.js
@@ -0,0 +1,63 @@
+
+d3.csv("Output_Data/Projected_tornadoes_reduced.csv").then(function (data) {
+
+    am4core.ready(function () {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+        // Create chart instance
+        var chart = am4core.create("chartdiv5", am4charts.XYChart3D);
+
+        // Add data
+        chart.data = data;
+
+        // Create axes
+        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "Year";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 50;
+
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.title.text = "Count";
+       
+
+        // Create series
+        var series3 = chart.series.push(new am4charts.ColumnSeries3D());
+        series3.dataFields.valueY = "Magnitude_2";
+        series3.dataFields.categoryX = "Year";
+        series3.name = "Magnitude 2 Tornadoes";
+        series3.clustered = false;
+        series3.columns.template.tooltipText = "{valueY}";
+        series3.columns.template.propertyFields.strokeDasharray = "dashLength";
+        series3.columns.template.propertyFields.fillOpacity = "alpha";
+        
+        var series = chart.series.push(new am4charts.ColumnSeries3D());
+        // series.dataFields.valueY = "Tornado_total";
+        series.dataFields.valueY = "Magnitude_0";
+        series.dataFields.categoryX = "Year";
+        series.name = "Magnitude 0 Tornadoes";
+        series.clustered = false;
+        series.columns.template.tooltipText = "{valueY} tornadoes";
+        series.columns.template.fillOpacity = 0.9;
+        series.columns.template.propertyFields.strokeDasharray = "dashLength";
+        series.columns.template.propertyFields.fillOpacity = "alpha";
+
+        var series2 = chart.series.push(new am4charts.ColumnSeries3D());
+        
+        series2.dataFields.valueY = "Tornado_total";
+        series2.dataFields.categoryX = "Year";
+        series2.name = "Total Tornadoes";
+        series2.clustered = false;
+        series2.columns.template.tooltipText = "{valueY}";
+        series2.columns.template.propertyFields.strokeDasharray = "dashLength";
+        series2.columns.template.propertyFields.fillOpacity = "alpha";
+
+      
+
+        // Add legend
+    chart.legend = new am4charts.Legend();
+
+    }); // end am4core.ready()
+})
