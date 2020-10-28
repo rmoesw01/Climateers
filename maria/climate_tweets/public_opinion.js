b8622cc6f7df294b9be4c5f403c52eb41f4cb926
Maria Dong <maria.j.dong@gmail.com>
2020-10-28 00:22:55 -0400

    complete sea temp site, fix sea ice replay bug, misc. edits

--- maria/climate_tweets/public_opinion.js
+++ maria/climate_tweets/public_opinion.js
@@ -1,133 +1,5 @@
-// // data of # of responses for each group
-// var q1_all = [2450, 348, 789];
-// var q1_rep = [517, 305, 653];
-// var q1_dem = [1861, 31, 118];
-
 // amcore theme for animation
 am4core.useTheme(am4themes_animated);
-
-// // main container
-// var mainContainer = am4core.create("chart1", am4core.Container);
-// mainContainer.width = am4core.percent(100);
-// mainContainer.height = am4core.percent(100);
-
-// // create pie chart
-// var partyChart = mainContainer.createChild(am4charts.PieChart);
-// partyChart.radius = am4core.percent(70);
-// partyChart.innerRadius = am4core.percent(40);
-
-// // add data
-// partyChart.data = [{
-//     // data for all respondents; no party affiliations yet
-//     "name": "[bold]not enough[/]",
-//     "fontColor": am4core.color("#2C3E50"),
-//     "radius": 20,
-//     "value": q1_all[0],
-//     // "tickDisabled": false,
-//     'sliceColor': am4core.color('#F1C40F')
-// }, {
-//     "name": "more than enough",
-//     "fontColor": am4core.color("#566573"),
-//     "radius": 20,
-//     "value": q1_all[1],
-//     // "tickDisabled": true,
-//     'sliceColor': am4core.color('#1F618D')
-// }, {
-//     "name": "just enough",
-//     "fontColor": am4core.color("#566573"),
-//     "radius": 30,
-//     "value": q1_all[2],
-//     // "labelDisabled": true,
-//     // "tickDisabled": true,
-//     'sliceColor': am4core.color('#154360')
-// }];
-
-// // create series from chart & data
-// var partySeries = partyChart.series.push(new am4charts.PieSeries());
-// partySeries.dataFields.value = "value";
-// partySeries.dataFields.category = "name";
-// partySeries.alignLabels = false;
-
-// // fill slices of pie chart w/ assigned color
-// partySeries.slices.template.propertyFields.fill = 'sliceColor';
-// // partySeries.ticks.template.propertyFields.disabled = "tickDisabled";
-
-// // config labels & tooltips
-// // partySeries.labels.template.text = "{category}";
-// partySeries.labels.template.propertyFields.fill = "fontColor";
-// // partySeries.labels.template.propertyFields.disabled = "labelDisabled";
-// partySeries.labels.template.radius = am4core.percent(-30);
-// partySeries.labels.template.propertyFields.radius = "radius";
-
-// partySeries.slices.template.tooltipText = "{value} responses";
-// partySeries.slices.template.cornerRadius = 8;
-
-// // create initial animation
-// partySeries.hiddenState.properties.endAngle = -90;
-
-// // create legend
-// // partyChart.legend = new am4charts.Legend();
-// // partyChart.legend.position = "right";
-
-// // add label
-// var label = partyChart.chartContainer.createChild(am4core.Label);
-// label.x = 10;
-// label.y = 100;
-// label.fill = am4core.color("#000000");
-// label.fontSize = 30;
-// label.fontWeight = "bold";
-// label.text = "All respondents";
-// label.fillOpacity = 0.3;
-// label.zIndex = 3;
-
-// // call fxn to populate republican data
-// partySeries.events.on("ready", function () {
-//     setTimeout(rep_fxn, 4000);
-// })
-
-// // fxn to populate republican data
-// function rep_fxn() {
-
-//     // change values of each slice accordingly
-//     partySeries.slices.each(function (slice) {
-//         slice.dataItem.value = q1_rep[slice.dataItem.index];
-//     })
-
-//     label.text = 'Republican/ \nlean Republican';
-//     label.y = -50;
-//     label.animate({ property: "y", to: 100 }, 300, am4core.ease.quadOut);
-
-//     // call fxn to populate democrat data
-//     setTimeout(dem_fxn, 4000);
-// }
-
-// // fxn to populate democrat data
-// function dem_fxn() {
-//     partySeries.slices.each(function (slice) {
-//         slice.dataItem.value = q1_dem[slice.dataItem.index];
-//     })
-
-//     label.text = 'Democrat/ \nlean Democrat';
-//     label.y = -50;
-//     label.animate({ property: "y", to: 100 }, 300, am4core.ease.quadOut);
-
-//     // call fxn to return to total data
-//     setTimeout(total_fxn, 4000);
-// }
-
-// // fxn to return to total data
-// function total_fxn() {
-//     partySeries.slices.each(function (slice) {
-//         slice.dataItem.value = q1_all[slice.dataItem.index];
-//     })
-
-//     label.text = 'All respondents';
-//     label.y = -50;
-//     label.animate({ property: "y", to: 100 }, 300, am4core.ease.quadOut);
-
-//     // call fxn to populate republican data
-//     setTimeout(rep_fxn, 4000);
-// }
 
 // begin pie chart for 2nd question
 var q2_all = [1889, 1014, 488, 190];
@@ -342,12 +214,6 @@ d3.csv('climate_tweets/pew_data/pew_csvs/global_survey.csv').then(response => {
     series.columns.template.column.cornerRadiusTopRight = 5;
 
     chart.cursor = new am4charts.XYCursor();
-    // chart.cursor.xAxis = categoryAxis;
-    // chart.cursor.yAxis = valueAxis;
-    // chart.cursor.lineY.stroke = am4core.color("#8F3985");
-    // chart.cursor.lineY.strokeWidth = 4;
-    // chart.cursor.lineY.strokeOpacity = 0.2;
-    // chart.cursor.lineY.strokeDasharray = "";
     chart.cursor.lineX.disabled = true;
     chart.cursor.behavior = 'zoomY';
 
@@ -386,12 +252,7 @@ d3.csv('climate_tweets/pew_data/pew_csvs/global_survey.csv').then(response => {
         chart.data = global_data;
 
         threat_label.text = '[bold]minor[/] threat';
-        // threat_label.animate({
-        //     'from': 0,
-        //     'to': 100,
-        //     'property': 'locationX'
-        // }, 1000, am4core.ease.quadInOut);
-
+        
         categoryAxis.sortBySeries = series;
 
         // call fxn to populate no threat data
@@ -406,8 +267,6 @@ d3.csv('climate_tweets/pew_data/pew_csvs/global_survey.csv').then(response => {
         chart.data = global_data;
 
         threat_label.text = '[bold]not[/] a threat';
-        // threat_label.y = -50;
-        // threat_label.animate({}, 300, am4core.ease.quadOut);
 
         categoryAxis.sortBySeries = series;
 
@@ -423,8 +282,6 @@ d3.csv('climate_tweets/pew_data/pew_csvs/global_survey.csv').then(response => {
         chart.data = global_data;
 
         threat_label.text = '[bold]major[/] threat';
-        // threat_label.y = -50;
-        // threat_label.animate({}, 300, am4core.ease.quadOut);
 
         categoryAxis.sortBySeries = series;
 
--- maria/sea_ice/sea_ice.js
+++ maria/sea_ice/sea_ice.js
@@ -1,31 +1,24 @@
-// retrieve geoJSON data for sea ice extent
-// for (var x = 1979; x < 2017; x++) {
-//     var url = `https://raw.githubusercontent.com/vigorousnorth/arctic-ice/master/geojson_files/extent_N_${x}08_geo.json`;
-// }
-
 am4core.ready (function() {
     // standard animated theme
     am4core.useTheme(am4themes_animated);
-    // am4core.useTheme(am4themes_amchartsdark);
     
     // create map instances
     var chart = am4core.create("chartdiv", am4maps.MapChart);
-    var chart2 = am4core.create("hiddenchartdiv", am4maps.MapChart);
 
     // set map definition as low
     chart.geodata = am4geodata_continentsLow;
-    chart2.geodata = am4geodata_continentsLow;
 
     // set projection as globe (orthographic)
     chart.projection = new am4maps.projections.Orthographic();
-    chart.panBehavior = "rotateLongLat";
-    chart2.projection = new am4maps.projections.Orthographic();
+    chart.panBehavior = 'rotateLongLat';
+
+    // disable panning/dragging of the map
+    // chart.seriesContainer.draggable = false;
+    // chart.seriesContainer.resizable = false;
 
     // set bg color
     chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#2E4053");
     chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 1.0;
-    chart2.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#2E4053");
-    chart2.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 1.0;
     
     // create map polygon series for countries
     var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
@@ -41,110 +34,326 @@ am4core.ready (function() {
     polygonTemplate.stroke = am4core.color("#212F3C");
     polygonTemplate.nonScalingStroke = true;
     
-    // // change fill color on hover; may not need if we're animating
-    // var hs = polygonTemplate.states.create("hover");
-    // hs.properties.fill = am4core.color("rgba(69, 179, 157, 1.0)");
-    
     // center on north pole
     chart.deltaLatitude = -90;
-    chart.homeZoomLevel = 2.7;
-    chart2.deltaLatitude = -90;
-    chart2.homeZoomLevel = 2.7;
+    chart.homeZoomLevel = 2.5;
+
+    // set max zoom level
+    chart.maxZoomLevel = 2.5;
+    // chart.minZoomLevel = 2.5;
     
-    // create map polygon series for northern polar sea ice
-    var iceSeries = chart.series.push(new am4maps.MapPolygonSeries());
-    var iceSeries2 = chart2.series.push(new am4maps.MapPolygonSeries());
+    // create outline for 1979 ice extent
+    var outlineSeries = chart.series.push(new am4maps.MapPolygonSeries());
 
     // add geojson url
-    iceSeries.geodataSource.url = 'https://raw.githubusercontent.com/vigorousnorth/arctic-ice/master/geojson_files/extent_N_197908_geo.json';
-    iceSeries2.geodataSource.url = 'https://raw.githubusercontent.com/vigorousnorth/arctic-ice/master/geojson_files/extent_N_201608_geo.json';
+    outlineSeries.geodataSource.url = 'sea_ice/geojsons/198009extent_geo.json';
+    outlineSeries.useGeodata = true;
 
     // config fill, stroke, color
-    var iceTemplate = iceSeries.mapPolygons.template;
+    var outlineTemplate = outlineSeries.mapPolygons.template;
+    // outlineTemplate.nonScalingStroke = true;
+    outlineTemplate.fillOpacity = 0.0;
+    outlineTemplate.strokeWidth = 2.0;
+    outlineTemplate.strokeDasharray = "5, 5";
 
-    console.log (iceSeries.mapPolygons);
-    // iceTemplate.tooltipText = "{name}";
+    // create map polygon series for northern polar sea ice
+    var iceSeries = chart.series.push(new am4maps.MapPolygonSeries());
+
+    // add geojson url
+    iceSeries.geodataSource.url = 'sea_ice/geojsons/198009extent_geo.json';
+    iceSeries.useGeodata = true;
+
+    // add *another* 1980 map polygon series for debugging
+    var iceSeries1 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries1.geodataSource.url = 'sea_ice/geojsons/198009extent_geo.json';
+    iceSeries1.useGeodata = true;
+    iceSeries1.hidden = true;
+
+    // create 1985 map polygon series
+    var iceSeries2 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries2.geodataSource.url = 'sea_ice/geojsons/198509extent_geo.json';
+    iceSeries2.useGeodata = true;
+    iceSeries2.hidden = true;
+
+    // create 1990 map polygon series
+    var iceSeries3 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries3.geodataSource.url = 'sea_ice/geojsons/199009extent_geo.json';
+    iceSeries3.useGeodata = true;
+    iceSeries3.hidden = true;
+
+    // create 1995 map polygon series
+    var iceSeries4 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries4.geodataSource.url = 'sea_ice/geojsons/199509extent_geo.json';
+    iceSeries4.useGeodata = true;
+    iceSeries4.hidden = true;
+
+    // create 2000 map polygon series
+    var iceSeries5 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries5.geodataSource.url = 'sea_ice/geojsons/200009extent_geo.json';
+    iceSeries5.useGeodata = true;
+    iceSeries5.hidden = true;
+
+    // create 2005 map polygon series
+    var iceSeries6 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries6.geodataSource.url = 'sea_ice/geojsons/200509extent_geo.json';
+    iceSeries6.useGeodata = true;
+    iceSeries6.hidden = true;
+
+    // create 2010 map polygon series
+    var iceSeries7 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries7.geodataSource.url = 'sea_ice/geojsons/201009extent_geo.json';
+    iceSeries7.useGeodata = true;
+    iceSeries7.hidden = true;
+
+    // create 2015 map polygon series
+    var iceSeries8 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries8.geodataSource.url = 'sea_ice/geojsons/201509extent_geo.json';
+    iceSeries8.useGeodata = true;
+    iceSeries8.hidden = true;
+
+    // create 2020 map polygon series
+    var iceSeries9 = chart.series.push(new am4maps.MapPolygonSeries());
+    iceSeries9.geodataSource.url = 'sea_ice/geojsons/202009extent_geo.json';
+    iceSeries9.useGeodata = true;
+    iceSeries9.hidden = true;
+    
+    // config fill, stroke, color
+    var iceTemplate = iceSeries.mapPolygons.template;
     iceTemplate.fill = am4core.color("rgba(255, 255, 255, 1.0)");
-    iceTemplate.fillOpacity = 0.9;
+    iceTemplate.fillOpacity = 1.0;
     iceTemplate.nonScalingStroke = true;
     iceTemplate.strokeWidth = 0.5;
+    iceTemplate.tooltipText = "sea ice extent: 7.67M sq km";
+
+    var iceTemplate1 = iceSeries1.mapPolygons.template;
+    iceTemplate1.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    iceTemplate1.fillOpacity = 1.0;
+    iceTemplate1.nonScalingStroke = true;
+    iceTemplate1.strokeWidth = 0.5;
+    iceTemplate1.tooltipText = "sea ice extent: 6.70M sq km";
 
     var iceTemplate2 = iceSeries2.mapPolygons.template;
-    // iceTemplate2.tooltipText = "{name}";
     iceTemplate2.fill = am4core.color("rgba(255, 255, 255, 1.0)");
-    iceTemplate2.fillOpacity = 0.9;
+    iceTemplate2.fillOpacity = 1.0;
     iceTemplate2.nonScalingStroke = true;
     iceTemplate2.strokeWidth = 0.5;
+    iceTemplate2.tooltipText = "sea ice extent: 6.70M sq km";
 
-    iceSeries.useGeodata = true;
-    iceSeries2.useGeodata = true;
+    var iceTemplate3 = iceSeries3.mapPolygons.template;
+    iceTemplate3.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    iceTemplate3.fillOpacity = 1.0;
+    iceTemplate3.nonScalingStroke = true;
+    iceTemplate3.strokeWidth = 0.5;
+    iceTemplate3.tooltipText = "sea ice extent: 6.14M sq km";
 
-    // add series for grid
-    var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
-    graticuleSeries.fitExtent = false;
-    graticuleSeries.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    var iceTemplate4 = iceSeries4.mapPolygons.template;
+    iceTemplate4.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    iceTemplate4.fillOpacity = 1.0;
+    iceTemplate4.nonScalingStroke = true;
+    iceTemplate4.strokeWidth = 0.5;
+    iceTemplate4.tooltipText = "sea ice extent: 6.08M sq km";
 
-    var slider = chart.createChild(am4core.Slider);
-    slider.padding(0, 15, 0, 60);
-    slider.background.padding(0, 15, 0, 60);
-    slider.marginBottom = 15;
-    slider.valign = "bottom";
+    var iceTemplate5 = iceSeries5.mapPolygons.template;
+    iceTemplate5.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    iceTemplate5.fillOpacity = 1.0;
+    iceTemplate5.nonScalingStroke = true;
+    iceTemplate5.strokeWidth = 0.5;
+    iceTemplate5.tooltipText = "sea ice extent: 6.25M sq km";
 
-    var currentIndex = -1;
-    // var colorset = new am4core.ColorSet();
+    var iceTemplate6 = iceSeries6.mapPolygons.template;
+    iceTemplate6.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    iceTemplate6.fillOpacity = 1.0;
+    iceTemplate6.nonScalingStroke = true;
+    iceTemplate6.strokeWidth = 0.5;
+    iceTemplate6.tooltipText = "sea ice extent: 5.5M sq km";
 
-    setInterval (function () {
-        var next = slider.start + 1 / (2016 - 1979 + 1);
-        if (next >= 1) {
-            next = 0;
-        }
+    var iceTemplate7 = iceSeries7.mapPolygons.template;
+    iceTemplate7.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    iceTemplate7.fillOpacity = 1.0;
+    iceTemplate7.nonScalingStroke = true;
+    iceTemplate7.strokeWidth = 0.5;
+    iceTemplate7.tooltipText = "sea ice extent: 4.87M sq km";
 
-        slider.animate ({ property: "start", to: next }, 300);
-        }, 2000)
+    var iceTemplate8 = iceSeries8.mapPolygons.template;
+    iceTemplate8.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    iceTemplate8.fillOpacity = 1.0;
+    iceTemplate8.nonScalingStroke = true;
+    iceTemplate8.strokeWidth = 0.5;
+    iceTemplate8.tooltipText = "sea ice extent: 4.62M sq km";
 
-    slider.events.on("rangechanged", function () {
-        changeYear();
-    })
-    
-    function changeYear() {
-        var totalYears = 2016 - 1979;
-        var yearIndex = Math.round(totalYears * slider.start);
-      
-        var morphToPolygon;
-      
-        if (currentIndex != yearIndex) {
+    var iceTemplate9 = iceSeries9.mapPolygons.template;
+    iceTemplate9.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    iceTemplate9.fillOpacity = 1.0;
+    iceTemplate9.nonScalingStroke = true;
+    iceTemplate9.strokeWidth = 0.5;
+    iceTemplate9.tooltipText = "sea ice extent: 3.92M sq km";
 
-        //   console.log (currentIndex, yearIndex);
-          iceSeries2.data = [];
-          iceSeries2.geodataSource.url = `https://raw.githubusercontent.com/vigorousnorth/arctic-ice/master/geojson_files/extent_N_${1979+yearIndex}08_geo.json`
-        //   console.log (iceSeries2.geodataSource.url);
-          
-          currentIndex = yearIndex;
-      
-          iceSeries2.events.once ("validated", function() {
-            console.log ('validated!');
-            morphToPolygon = iceSeries2.mapPolygons.getIndex(0);
-            console.log (morphToPolygon);
+    // unfortunately, looping through the templates didn't work. likely d/t the geojsons
 
-            if (morphToPolygon) {
-              console.log ('again');
-              var icePolygon = iceSeries.mapPolygons.getIndex(0);
-      
-              var morpher = icePolygon.polygon.morpher;
-              var morphAnimation = morpher.morphToPolygon(morphToPolygon.polygon.points);
-      
-            //   var colorAnimation = icePolygon.animate({ "property": "fill", "to": colorset.getIndex(Math.round(Math.random() * 20)) }, 1000);
-      
-            //   var animation = label.animate({ property: "y", to: 1000 }, 300);
-      
-            //   animation.events.once("animationended", function () {
-            //     label.text = morphToPolygon.dataItem.dataContext["name"];
-            //     label.y = -50;
-            //     label.animate({ property: "y", to: 200 }, 300, am4core.ease.quadOut);
-            //   })
-            }
-          })
-        }
+    // var year_list = [1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020];
+    // var series_list = [iceSeries, iceSeries2, iceSeries3, iceSeries4, iceSeries5, iceSeries6, iceSeries7, iceSeries8, iceSeries9];
+    // var extent_areas = ['7.67', '6.7', '6.14', '6.08', '6.25', '5.5', '4.87', '4.62', '3.92'];
+
+    // for (var x = 0; x < length.year_list; x++) {
+
+    //   // config fill, stroke, color
+    //   var thisSeries = series_list[x];
+    //   thisSeries.geodataSource.url = `geojsons/${year_list[x]}09extent_geo.json`;
+    //   thisSeries.useGeodata = true;
+
+    //   var iceTemplate = thisSeries.mapPolygons.template;
+    //   iceTemplate.tooltipText = `sea ice extent: ${extent_areas[x]}`;
+    //   iceTemplate.fill = am4core.color("rgba(255, 255, 255, 1.0)");
+    //   iceTemplate.fillOpacity = 1.0;
+    //   iceTemplate.nonScalingStroke = true;
+    //   iceTemplate.strokeWidth = 0.5;
+
+    //   if (x != 0) {
+    //     thisSeries.hidden = true;
+    //   }
+    //   chart.invalidateData();
+    // }
+
+    // create labels for years
+    var iceLabel = chart.chartContainer.createChild(am4core.Label);
+    iceLabel.isMeasured = false;
+    iceLabel.x = 10;
+    iceLabel.y = 10;
+    iceLabel.fill = am4core.color("#808B96");
+    iceLabel.fontSize = 30;
+    iceLabel.fontWeight = "bold";
+    iceLabel.text = "1980";
+    iceLabel.fillOpacity = 0.8;
+    iceLabel.zIndex = 3;
+
+    // create triangle for play button
+    var triangle2 = new am4core.Triangle();
+    triangle2.width = 8;
+    triangle2.height = 10;
+    triangle2.fill = am4core.color("#212F3C");
+    triangle2.direction = "right";
+    triangle2.valign = "middle";
+    triangle2.align = "center";
+    triangle2.dx = 1;
+
+    // create button for replaying
+    replayButton = chart.createChild(am4core.Button);
+    replayButton.horizontalCenter = "middle";
+    replayButton.verticalCenter = "middle";
+    replayButton.padding(0, 0, 0, 0);
+    replayButton.background.cornerRadius(25, 25, 25, 25);
+    replayButton.x = 30;
+    replayButton.y = 470;
+    replayButton.dy = 1;
+    replayButton.height = 40;
+    replayButton.width = 40;
+    replayButton.zIndex = 5000;
+    replayButton.icon = triangle2;
+    replayButton.hide();
+
+    // create label for button
+    var buttonLabel = chart.chartContainer.createChild(am4core.Label);
+    buttonLabel.isMeasured = false;
+    buttonLabel.x = 60;
+    buttonLabel.y = 452;
+    buttonLabel.fill = am4core.color("#808B96");
+    buttonLabel.fontSize = 30;
+    buttonLabel.fontWeight = "bold";
+    buttonLabel.text = "replay";
+    buttonLabel.fillOpacity = 0.9;
+    buttonLabel.zIndex = 3;
+    buttonLabel.hide();
+
+    // call restart fxn when button is clicked
+    replayButton.events.on("hit", morphIce);
+
+    // start animation sequence when ready
+    chart.events.on ('ready', function() {
+      setTimeout (morphIce, 500)
+    });
+
+    function morphIce() {
+      iceSeries.hide();
+      iceSeries1.show();
+      iceLabel.text = '1980';
+
+      replayButton.hide();
+      buttonLabel.hide();
+
+      setTimeout (morphIce1, 3000)
+    }
+
+    function morphIce1() {
+      iceSeries1.hide();
+      iceSeries2.show();
+      iceLabel.text = '1985';
+      iceLabel.y = -30;
+      iceLabel.animate({ property: "y", to: 10 }, 300, am4core.ease.quadOut);
+      setTimeout (morphIce2, 3000)
+    }
+
+    function morphIce2() {
+      iceSeries2.hide();
+      iceSeries3.show();
+      iceLabel.text = '1990';
+      iceLabel.y = -30;
+      iceLabel.animate({ property: "y", to: 10 }, 300, am4core.ease.quadOut);
+      setTimeout (morphIce3, 3000)
+    }
+
+    function morphIce3() {
+      iceSeries3.hide();
+      iceSeries4.show();
+      iceLabel.text = '1995';
+      iceLabel.y = -30 ;
+      iceLabel.animate({ property: "y", to: 10 }, 300, am4core.ease.quadOut);
+      setTimeout (morphIce4, 3000)
+    }
+
+    function morphIce4() {
+      iceSeries4.hide();
+      iceSeries5.show();
+      iceLabel.text = '2000';
+      iceLabel.y = -30;
+      iceLabel.animate({ property: "y", to: 10 }, 300, am4core.ease.quadOut);
+      setTimeout (morphIce5, 3000)
+    }
+
+    function morphIce5() {
+      iceSeries5.hide();
+      iceSeries6.show();
+      iceLabel.text = '2005';
+      iceLabel.y = -30;
+      iceLabel.animate({ property: "y", to: 10 }, 300, am4core.ease.quadOut);
+      setTimeout (morphIce6, 3000)
+    }
+
+    function morphIce6() {
+      iceSeries6.hide();
+      iceSeries7.show();
+      iceLabel.text = '2010';
+      iceLabel.y = -30;
+      iceLabel.animate({ property: "y", to: 10 }, 300, am4core.ease.quadOut);
+      setTimeout (morphIce7, 3000)
+    }
+
+    function morphIce7() {
+      iceSeries7.hide();
+      iceSeries8.show();
+      iceLabel.text = '2015';
+      iceLabel.y = -30;
+      iceLabel.animate({ property: "y", to: 10 }, 300, am4core.ease.quadOut);
+      setTimeout (morphIce8, 3000)
+    }
+
+    function morphIce8() {
+      iceSeries8.hide();
+      iceSeries9.show();
+      iceLabel.text = '2020';
+      iceLabel.y = -30;
+      iceLabel.animate({ property: "y", to: 10 }, 300, am4core.ease.quadOut);
+      replayButton.show();
+      buttonLabel.show();
     }
 
 }); // end am4core.ready()
