8dde48cc3881dc0cf43bf6e79429a5e6e5109e8d
Becki Oeswein Leeds <rmoesw01@gmail.com>
2020-10-29 12:18:37 -0400

    clean up precip chart js code

--- merged/assets/js/custom/precipPyramid.js
+++ merged/assets/js/custom/precipPyramid.js
@@ -12,69 +12,63 @@ am4core.ready(function () {
     d3.json("assets/data/json/snow_prcp.json").then(function(currData) {
         var precipData = currData;
         console.log(precipData);
-      
 
-    var maleChart = mainContainer.createChild(am4charts.XYChart);
-    maleChart.paddingRight = 0;
-    maleChart.data = JSON.parse(JSON.stringify(precipData));
-
-    // Create axes
-    var maleCategoryAxis = maleChart.yAxes.push(new am4charts.CategoryAxis());
-    maleCategoryAxis.dataFields.category = "year";
-    maleCategoryAxis.renderer.grid.template.location = 0;
-    //maleCategoryAxis.renderer.inversed = true;
-    maleCategoryAxis.renderer.minGridDistance = 15;
-
-    var maleValueAxis = maleChart.xAxes.push(new am4charts.ValueAxis());
-    maleValueAxis.renderer.inversed = true;
-    maleValueAxis.min = 0;
-    maleValueAxis.max = 2500;
-    maleValueAxis.strictMinMax = true;
-
-    maleValueAxis.numberFormatter = new am4core.NumberFormatter();
-    maleValueAxis.numberFormatter.numberFormat = "#";
-
-    // Create series
-    var maleSeries = maleChart.series.push(new am4charts.ColumnSeries());
-    maleSeries.dataFields.valueX = "snow fall";
-    // maleSeries.dataFields.valueXShow = "percent";
-    maleSeries.calculatePercent = true;
-    maleSeries.dataFields.categoryY = "year";
-    maleSeries.interpolationDuration = 1000;
-    maleSeries.columns.template.tooltipText = "Snow Fall{categoryY}: {valueX}";
-    //maleSeries.sequencedInterpolation = true;
-
-
-    var femaleChart = mainContainer.createChild(am4charts.XYChart);
-    femaleChart.paddingLeft = 0;
-    femaleChart.data = JSON.parse(JSON.stringify(precipData));
+    var snowChart = mainContainer.createChild(am4charts.XYChart);
+    snowChart.paddingRight = 0;
+    snowChart.data = JSON.parse(JSON.stringify(precipData));
 
     // Create axes
-    var femaleCategoryAxis = femaleChart.yAxes.push(new am4charts.CategoryAxis());
-    femaleCategoryAxis.renderer.opposite = true;
-    femaleCategoryAxis.dataFields.category = "year";
-    femaleCategoryAxis.renderer.grid.template.location = 0;
-    femaleCategoryAxis.renderer.minGridDistance = 15;
+    var snowCategoryAxis = snowChart.yAxes.push(new am4charts.CategoryAxis());
+    snowCategoryAxis.dataFields.category = "year";
+    snowCategoryAxis.renderer.grid.template.location = 0;
+    //snowCategoryAxis.renderer.inversed = true;
+    snowCategoryAxis.renderer.minGridDistance = 15;
 
-    var femaleValueAxis = femaleChart.xAxes.push(new am4charts.ValueAxis());
-    femaleValueAxis.min = 0;
-    femaleValueAxis.max = 2500;
-    femaleValueAxis.strictMinMax = true;
-    femaleValueAxis.numberFormatter = new am4core.NumberFormatter();
-    femaleValueAxis.numberFormatter.numberFormat = "#";
-    femaleValueAxis.renderer.minLabelPosition = 0.01;
+    var snowValueAxis = snowChart.xAxes.push(new am4charts.ValueAxis());
+    snowValueAxis.renderer.inversed = true;
+    snowValueAxis.min = 0;
+    snowValueAxis.max = 2500;
+    snowValueAxis.strictMinMax = true;
+
+    snowValueAxis.numberFormatter = new am4core.NumberFormatter();
+    snowValueAxis.numberFormatter.numberFormat = "#";
 
     // Create series
-    var femaleSeries = femaleChart.series.push(new am4charts.ColumnSeries());
-    femaleSeries.dataFields.valueX = "precipitation";
-    // femaleSeries.dataFields.valueXShow = "percent";
-    femaleSeries.calculatePercent = true;
-    femaleSeries.fill = femaleChart.colors.getIndex(4);
-    femaleSeries.stroke = femaleSeries.fill;
-    //femaleSeries.sequencedInterpolation = true;
-    femaleSeries.columns.template.tooltipText = "Precipitation{categoryY}: {valueX}";
-    femaleSeries.dataFields.categoryY = "year";
-    femaleSeries.interpolationDuration = 1000;
+    var snowSeries = snowChart.series.push(new am4charts.ColumnSeries());
+    snowSeries.dataFields.valueX = "snow fall";
+    snowSeries.calculatePercent = true;
+    snowSeries.dataFields.categoryY = "year";
+    snowSeries.interpolationDuration = 1000;
+    snowSeries.columns.template.tooltipText = "Snow Fall {categoryY}: {valueX}";
+
+    var precipChart = mainContainer.createChild(am4charts.XYChart);
+    precipChart.paddingLeft = 0;
+    precipChart.data = JSON.parse(JSON.stringify(precipData));
+
+    // Create axes
+    var precipCategoryAxis = precipChart.yAxes.push(new am4charts.CategoryAxis());
+    precipCategoryAxis.renderer.opposite = true;
+    precipCategoryAxis.dataFields.category = "year";
+    precipCategoryAxis.renderer.grid.template.location = 0;
+    precipCategoryAxis.renderer.minGridDistance = 15;
+
+    var precipValueAxis = precipChart.xAxes.push(new am4charts.ValueAxis());
+    precipValueAxis.min = 0;
+    precipValueAxis.max = 2500;
+    precipValueAxis.strictMinMax = true;
+    precipValueAxis.numberFormatter = new am4core.NumberFormatter();
+    precipValueAxis.numberFormatter.numberFormat = "#";
+    precipValueAxis.renderer.minLabelPosition = 0.01;
+
+    // Create series
+    var precipSeries = precipChart.series.push(new am4charts.ColumnSeries());
+    precipSeries.dataFields.valueX = "precipitation";
+    precipSeries.calculatePercent = true;
+    precipSeries.fill = precipChart.colors.getIndex(4);
+    precipSeries.stroke = precipSeries.fill;
+    precipSeries.columns.template.tooltipText = "Precipitation {categoryY}: {valueX}";
+    precipSeries.dataFields.categoryY = "year";
+    precipSeries.interpolationDuration = 1000;
 
 
     var label = mainContainer.createChild(am4core.Label);
