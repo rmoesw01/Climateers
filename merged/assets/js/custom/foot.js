bfc1a7ef72396f0c4ac821e8b4011efc0e90e41b
Maria Dong <maria.j.dong@gmail.com>
2020-10-29 15:01:44 -0400

    finish formatting for awareness page

--- merged/assets/js/custom/foot.js
+++ merged/assets/js/custom/foot.js
@@ -7,8 +7,17 @@ d3.csv('assets/data/Emissions/footChart.csv').then(function (data) {
 
         var iconPath = "M29.166,23.822C28.352,15.234,25.026,8.196,18.409,9.5c-4.322,0.852-7.935,6.903-9.49,15.586c-0.883,4.945-0.067,10.661,0.962,16.108c1.23,6.519-0.495,15.575,1.451,20.901c1.583,4.334,4.084,5.684,7.077,5.773c2.992,0.089,6.111-1.316,7.416-5.724c2.567-8.646,0.218-13.294,0.499-21.378C26.524,34.925,29.876,31.349,29.166,23.822z"
 
-        var chart = am4core.create("chart1", am4charts.SlicedChart);
+        var container = am4core.create("chart1", am4core.Container);
+        container.width = am4core.percent(100);
+        container.height = am4core.percent(100);
+
+        var chart = container.createChild(am4charts.SlicedChart);
+        chart.width = am4core.percent(90);
+        chart.height = am4core.percent(100);
         chart.paddingTop = am4core.percent(10);
+
+        // var chart = am4core.create("chart1", am4charts.SlicedChart);
+        // chart.paddingTop = am4core.percent(10);
 
         chart.data = data
 
